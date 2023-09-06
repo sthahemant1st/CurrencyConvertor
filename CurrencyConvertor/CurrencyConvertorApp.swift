79a075debefe57c61ce1e067bcdcd918d0747bdf
Hemant Shrestha <sthahemant1st@gmail.com>
2023-09-06 21:32:25 +0545

    Initial Commit

---
+++ CurrencyConvertor/CurrencyConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConvertorApp.swift
+//  CurrencyConvertor
+//
+//  Created by Hemant Shrestha on 06/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
