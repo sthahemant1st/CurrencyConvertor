0422cad494cb7cfcffa5de1e1e7274a4a9a9b99a
Hemant Shrestha <hemant.shrestha@f1soft.com>
2023-09-08 21:09:02 +0545

    removes forceCast with throw in JSONHelper

--- CurrencyConvertor/Utilities/JSONHelper.swift
+++ CurrencyConvertor/Utilities/JSONHelper.swift
@@ -9,10 +9,15 @@ import Foundation
 
 struct JSONHelper {
     static func convert<T: Decodable>(name: String, type: T.Type) throws -> T {
-        let url = Bundle.main.url(forResource: name, withExtension: "json")!
+        let url = Bundle.main.url(forResource: name, withExtension: "json")
+        guard let url else { throw JSONHelpError.fileNotFound }
         let data = try Data(contentsOf: url)
         let decoder = JSONDecoder()
         let products = try decoder.decode(T.self, from: data)
         return products
+    }
+    
+    enum JSONHelpError: Error {
+        case fileNotFound
     }
 }
