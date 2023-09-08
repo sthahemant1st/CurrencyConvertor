12fa68e52924ae09b77dcbe46c29c9c013a6a3fd
Hemant Shrestha <hemant.shrestha@f1soft.com>
2023-09-08 20:24:50 +0545

    adds ErrorRetryView

--- CurrencyConvertor/Reuseable/View/ErrorRetryView.swift
+++ CurrencyConvertor/Reuseable/View/ErrorRetryView.swift
@@ -8,11 +8,55 @@
 import SwiftUI
 
 struct ErrorRetryView: View {
+    let title: String
+    let description: String
+    let actionText: LocalizedStringKey
+    let action: VoidFunction
+    
+    init(
+        title: String = "Error!",
+        description: String,
+        actionText: LocalizedStringKey = "Retry",
+        action: @escaping VoidFunction
+    ) {
+        self.title = title
+        self.description = description
+        self.actionText = actionText
+        self.action = action
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            Spacer()
+            Image(systemName: "exclamationmark.octagon.fill")
+                .resizable()
+                .renderingMode(.template)
+                .frame(size: 100)
+                .foregroundStyle(.red)
+            Text(title)
+                .font(.title)
+            
+            Text(description)
+                .fixedSize(horizontal: false, vertical: true)
+                .multilineTextAlignment(.center)
+
+            Spacer()
+            
+            Button(actionText, action: action)
+                .buttonStyle(.borderedProminent)
+                .tint(.appPrimary)
+            
+        }
+        .padding(.all, 16)
     }
 }
 
-#Preview {
-    ErrorRetryView()
+struct ErrorRetryView_Previews: PreviewProvider {
+    static var previews: some View {
+        ErrorRetryView(
+            description: "This the description for error",
+            actionText: "Retry",
+            action: {}
+        )
+    }
 }
